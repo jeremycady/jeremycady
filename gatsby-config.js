00a1024a252d8d3b30be34946ec33ba73ea90da2
jeremycady <jeremy@mx103.com>
2020-10-26 08:53:05 -0500

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,47 @@
+const dotenv = require("dotenv");
+
+if (process.env.ENVIRONMENT !== "production") {
+  dotenv.config();
+}
+
+const { spaceId, accessToken } = process.env;
+
+module.exports = {
+  siteMetadata: {
+    title: `Jeremy Cady`,
+    description: `Where I share what is important to me - religion, family, politics, technology, finances, personal development.`,
+    author: `@jeremycady`
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`
+      }
+    },
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId,
+        accessToken
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Jeremy Cady`,
+        short_name: `Jeremy Cady`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#333`,
+        icon: `src/images/fav_icon.png` // This path is relative to the root of the site.
+      }
+    }, // To learn more, visit: https://gatsby.dev/offline // this (optional) plugin enables Progressive Web App + Offline functionality
+    // `gatsby-plugin-offline`
+  ]
+};
